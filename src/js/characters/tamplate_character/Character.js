55c6dbe58c0a24886c9e7022bb04f0d03ff10bf1
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-05-02 16:30:57 +0300

    install packages

--- src/js/characters/tamplate_character/Character.js
+++ src/js/characters/tamplate_character/Character.js
@@ -30,4 +30,21 @@ export class Character {
         throw new Error('неверно указан тип персонажа');
     }
   }
+
+  levelUp() {
+
+    if (this.health <= 0) {
+      throw new Error('нельзя повысить левел умершего');
+    }
+    this.level += 1;
+    this.attack *= 1.2 ;
+    this.defence *= 1.2 ;
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - defence / 100);
+    }
+  }
 }
